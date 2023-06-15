825de90b0333faf5870998f012a00e077649eb18
¨Tpericles¨ <thiagopericles@outlook.com>
2023-06-14 22:53:08 -0300

    Atualizando imagens

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ function togglemode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute(
-      "src",
-      "/assests/Imagem do WhatsApp de 2023-06-01 à(s) 22.03 1tp.png"
-    )
+    img.setAttribute("src", "/Retorno/assests/tp.png")
   } else {
-    img.setAttribute("src", "/assests/avatartp.png")
+    img.setAttribute("src", "/Retorno/assests/avatartp.png")
   }
 }
